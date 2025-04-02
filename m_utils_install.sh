dff6c9445f781c6cdac973083045b97de2b35a07
Mara Oprea <oprea.mara03@gmail.com>
2025-04-02 11:01:50 +0300

    m_utils

--- m_utils_install.sh
+++ m_utils_install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 git clone https://github.com/mara291/Sheepsay.git
-sudo cp sheepsay /usr/local/bin/sheepsay
-sudo cp sheepsay.1 /usr/share/man/man1 
+./Sheepsay/add_to_man
 
 git clone https://github.com/mara291/Deadline.git
-sudo cp deadline /usr/local/bin/deadline
-sudo cp deadline.1 /usr/share/man/man1  
-mkdir ~/.config/Deadline
-touch ~/.config/Deadline/ddl.txt
+./Deadline/add_to_man
 
 git clone https://github.com/mara291/Temperature.git
-sudo cp temperature /usr/local/bin/temperature
-sudo cp temperature.1 /usr/share/man/man1
+./Temperature/add_to_man
 
 sudo mandb
